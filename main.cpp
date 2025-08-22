c1487b97ccc5d5c49d69bcd44c2a33d4329c741c
Eric Bush <eric.bush@mailbox.tu-dresden.de>
2025-08-22 17:01:44 +0200

    Show window

--- main.cpp
+++ main.cpp
@@ -1,18 +1,13 @@
-#include <iostream>
+#include <QMainWindow>
+#include <QApplication>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main()
+int main(int argc, char* argv[])
 {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    QApplication app(argc, argv);
 
-    for (int i = 1; i <= 5; i++)
-    {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
+    QMainWindow window;
 
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+    window.show();
+
+    return app.exec();
 }
