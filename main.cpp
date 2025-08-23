bc4acf582ef1e73709cc7a13fa79742f14ccebc3
Eric Bush <eric.bush@mailbox.tu-dresden.de>
2025-08-23 14:50:18 +0200

    Display keypad

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
-#include <QMainWindow>
-#include <QApplication>
+#include "ui_calculator_window.h"
+#include "ui_keypad.h"
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
 
     QMainWindow window;
+    QFrame keypad;
 
+
+    Ui::FinancialCalculatorWindow fin_calc_window;
+    Ui::KeypadFrame keypad_frame;
+
+    keypad_frame.setupUi(&keypad);
+    fin_calc_window.setupUi(&window);
+
+    window.setCentralWidget(&keypad);
     window.show();
 
     return app.exec();
